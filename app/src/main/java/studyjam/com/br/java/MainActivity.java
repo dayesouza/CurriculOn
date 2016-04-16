905edf977059158c334f22fd1b58572fe453e2b3
Day Souza <day.carol18@gmail.com>
2016-04-16 15:18:24 -0300

    ultimas alterações nas telas

--- app/src/main/java/studyjam/com/br/java/MainActivity.java
+++ app/src/main/java/studyjam/com/br/java/MainActivity.java
@@ -1,15 +1,24 @@
 package studyjam.com.br.java;
 
 import android.app.DialogFragment;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import studyjam.com.br.fragments.DatePickerFragment;
 
 public class MainActivity extends AppCompatActivity {
+    //Para contar as telas de instrucao
+    int view_instrucao = 2;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,16 +33,12 @@ public class MainActivity extends AppCompatActivity {
         DialogFragment newFragment = new DatePickerFragment();
         newFragment.show(getFragmentManager(), "DatePicker");
     }
-    public void editar(View view){
+
+    public void avancaTelaInfoPessoais(View view){
         //Seta a view de informações pessoais
         setContentView(R.layout.informacoes_pessoais_1);
 
         criaSpinner(R.id.spinner_UF, R.array.uf_array);
-    }
-
-    public void avancaTelaInfoPessoais(View view){
-        //vem depois das instruções
-        //setContentView(R.layout.informacoes_pessoais_1);
     }
 
     public void avancaTelaInfoEstudos(View view){
@@ -77,5 +82,73 @@ public class MainActivity extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Aplica o Adapter no Spinner
         spinner.setAdapter(adapter);
+    }
+
+    public void tela_instrucao(View view){
+        setContentView(R.layout.instrucoes);
+    }
+
+    /**
+     * Muda a tela de instrução conforme é clicado na tela
+     * @param view
+     */
+    public void avancaInstrucao(View view){
+        //Pega a imagem de demonstração
+        ImageView img_instrucao = (ImageView) findViewById(R.id.img_tela);
+        //Pega o texto
+        TextView texto_instrucao = (TextView) findViewById(R.id.texto_instrucao);
+        //Apaga o texto de descrição do app
+        TextView texto_descricao = (TextView) findViewById(R.id.texto_descricao);
+        texto_descricao.setText("");
+
+        //Seta a imagem e o texto de acordo com a quantidade de instruções lidas
+        int r_img = 0;
+        String msg = "";
+        int contador_instr = 0;
+        switch(view_instrucao){
+            case 2:
+                r_img = R.drawable.tela_2;
+                msg = "Depois, você informa sua escolaridade, seus cursos...";
+                contador_instr = R.id.instr_2;
+                break;
+            case 3:
+                r_img = R.drawable.tela_3;
+                msg = "Aqui você coloca suas experiências...";
+                contador_instr = R.id.instr_3;
+                break;
+            case 4:
+                r_img = R.drawable.tela_4;
+                msg = "E aqui as informações adicionais! Pode colocar coisas sobre você, seus conhecimentos e o que você" +
+                        " acha que o entrevistados deve saber.";
+                contador_instr = R.id.instr_4;
+                break;
+            case 5:
+                r_img = R.drawable.compartilhamento;
+                msg = "Daí é só sair compartilhando e boa sorte!";
+                contador_instr = R.id.instr_5;
+                break;
+        }
+        img_instrucao.setImageResource(r_img);
+        texto_instrucao.setText(msg);
+
+        //Pega o contador das telas de instrução e colore como concluído
+        TextView cont_instrucao = (TextView) findViewById(contador_instr);
+        cont_instrucao.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+
+        if (view_instrucao == 5){
+            Button botao_start = (Button) findViewById(R.id.botao_comecar);
+            botao_start.setVisibility(View.VISIBLE);
+            botao_start.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    avancaTelaInfoPessoais(v);
+                }
+            });
+            LinearLayout linear = (LinearLayout) findViewById(R.id.layout_intrucao);
+            linear.setOnClickListener(null);
+        }
+
+        view_instrucao++;
+//        LinearLayout linear
     }
 }
